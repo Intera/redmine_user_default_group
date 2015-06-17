702aadd19dcf42dd389a07ed9327cee30fc14ac2
Alexander Stehlik <alexander.stehlik@gmail.com>
2015-06-17 20:47:38 +0200

    [TASK] Initial version

---
+++ init.rb
@@ -0,0 +1,15 @@
+require 'redmine'
+require 'redmine_user_default_group/user_patch'
+
+Redmine::Plugin.register :redmine_user_default_group do
+  name 'Redmine Default Group plugin'
+  author 'Intera GmbH'
+  description 'This plugin assigns a default group to newly created users.'
+  version '0.0.1'
+  url ''
+  settings :partial => 'settings/default_group',
+           :default => {
+               :group => false,
+           }
+
+end
