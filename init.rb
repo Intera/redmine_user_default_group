24954363803a6b7c1b0b3d185f90559d558327b9
Alexander Stehlik <alexander.stehlik@gmail.com>
2015-07-06 13:49:11 +0200

    [TASK] Add URL to Github in init.rb

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_user_default_group do
   author 'Intera GmbH'
   description 'This plugin assigns a default group to newly created users.'
   version '0.0.1'
-  url ''
+  url 'https://github.com/Intera/redmine_user_default_group'
   settings :partial => 'settings/default_group',
            :default => {
                :group => false,
